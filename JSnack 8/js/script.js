682d579f1d8e245ffe51dea364fa0b440569c4f0
nvaccina <niccolovac@gmail.com>
2023-03-27 16:45:30 +0200

    Esercizi 5-6-7 e inizio 8

--- JSnack 8/js/script.js
+++ JSnack 8/js/script.js
@@ -2,14 +2,13 @@
 Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 In questo caso possiamo sfruttare a nostro vantaggio il fatto che la funzione prompt restituisca una stringa anche se si è digitato un numero e recuperare una cifra alla volta usando il ciclo for per scorrere la stringa numerica
 */
-let sommaWhile = 0;
-let counter = '';
 
-while (counter < 4){
+let sommaWhile = 0;
+
+for(let i = 0; i < 4; i++){
   const numeri = parseInt(prompt('Inserisci un numero di 4 cifre'));
   let risultato = somma(numeri);
-
-  counter++;
+  
   console.log(numeri);
 }
 
